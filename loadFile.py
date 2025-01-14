0c507871241a369e4a64361613f0f943e5395781
chalmejd-TEAM <151769819+chalmejd-TEAM@users.noreply.github.com>
2025-01-14 10:33:20 -0600

    Create json of channel list for front end

--- loadFile.py
+++ loadFile.py
@@ -4,13 +4,22 @@ import pandas
 import fast_calculations
 import json
 
+def loadChannelList(file):
+    try:
+        with dw.open(file) as dataFile:
+            channelList = [channel for channel in dataFile
+                        if not 'CAN' in dataFile[channel].channel_index]
+        return channelList
+
+    except ValueError:
+        pass
+
 def loadFile(file):
     try:
         with dw.open(file) as dataFile:
             channelList = [channel for channel in dataFile
                         if not 'CAN' in dataFile[channel].channel_index]
             df = dataFile.dataframe(channelList)
-        print(dataFile.info)
         return df
 
     except ValueError:
@@ -20,18 +29,24 @@ with open('testFile.json', 'r') as file:
     data = json.load(file)
     fileName = data["fileName"]
     exponents = data["exponents"]
+    loadChannel = data["loadChannel"]
+    revChannel = data["revChannel"]
+
+channelList = loadChannelList(fileName)
+
+# Convert channel list into JSON
+# File name is channelList.json
+with open("channelList.json", "w") as final:
+	json.dump(channelList, final)
 
 df = loadFile(fileName)
 
-print(list(df))
-
-load = df['Combined Load'].to_numpy()
-revs = df['CNT 1/Raw_Count'].to_numpy()
+load = df[loadChannel].to_numpy()
+revs = df[revChannel].to_numpy()
 
 
-# Example usage
-torque_smoothed = abs(np.array(load))  # Replace with your data
-rev_count = np.array(revs)        # Replace with your data
+torque_smoothed = abs(np.array(load))  
+rev_count = np.array(revs)       
 
 torque_smoothed = np.nan_to_num(torque_smoothed, nan=0.0)  # Replace NaN with 0
 rev_count = np.nan_to_num(rev_count, nan=0.0)             # Replace NaN with 0
