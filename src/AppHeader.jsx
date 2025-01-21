cb9c53dec268f3bd8a5847ecafe4271f6cdeee2c
chalmejd-TEAM <151769819+chalmejd-TEAM@users.noreply.github.com>
2025-01-21 08:52:26 -0600

    Adjustments to header styling

--- src/AppHeader.jsx
+++ src/AppHeader.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './AppHeader.css';
 
 const AppHeader = () => {
   return (
     <header style={headerStyle}>
-      <h1 style={titleStyle}>Dewesoft Analyze</h1>
+      <h1 className='gradient-text'>Dewesoft Analyze</h1>
     </header>
   );
 };
@@ -14,15 +15,10 @@ const headerStyle = {
   top: 0,               // Aligns it to the top of the screen
   left: 0,              // Aligns it to the left edge of the screen
   width: '100%',        // Makes the header full width
-  padding: '20px 0',
+  padding: '25px 0',
   textAlign: 'center',
   color: '#dddddd',            // White text
   zIndex: 1000,             // Ensures the header stays on top of other content
-};
-
-const titleStyle = {
-  margin: 0,
-  fontSize: '2.5em', // Adjust the size of the title as needed
 };
 
 export default AppHeader;
